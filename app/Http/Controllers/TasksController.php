83c5a67836e9bff6d05e8541b1da7b2b27700add
Daniel Ortiz <danielortiz@ballastlane.com>
2023-12-18 12:17:53 -0500

    feat(tasks): use softDeletes

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -19,13 +19,7 @@ class TasksController extends Controller
         return response($task->jsonSerialize(), Response::HTTP_OK);
     } 
 
-    public function destroy(Task $task)
-    {
-        $task->delete();
-
-        return response(null, Response::HTTP_OK);
-    }
-
+    
     public function update(Request $request, Task $task)
     {
         $task->update($request->all());
@@ -41,5 +35,12 @@ class TasksController extends Controller
         ]);
 
         return response($task->jsonSerialize(), Response::HTTP_CREATED);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response(null, Response::HTTP_OK);
     }
 }
